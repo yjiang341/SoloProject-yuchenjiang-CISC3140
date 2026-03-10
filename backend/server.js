67fed3971ae1ce6fd73de22659ce3966936a7369
Shirokome341 <yuchen.jiang103@gmail.com>
2026-03-09 21:27:36 -0400

    server.js updated

--- backend/server.js
+++ backend/server.js
@@ -2,4 +2,12 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 
-app.get("/", (req, res) => {})
+app.get("/", (req, res) => {
+    res.send("Welcome to the server");
+})
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`);
+})
