f037e9a1042db37588a97b7ca1d983c852bdb6ff
Shirokome341 <yuchen.jiang103@gmail.com>
2026-03-09 22:08:32 -0400

    vite created

--- frontend/demo/App1.jsx
+++ frontend/demo/App1.jsx
@@ -7,19 +7,25 @@ function App() {
 	const [count, setCount] = useState(0);
 
 	useEffect(() => {
-		//fetch data from the backend
-		function async fetchData() {
-			async function fetchData() {
+		// fetch data from the backend
+		async function fetchData() {
+			try {
 				const response = await fetch("https://localhost:3001/");
+				const data = await response.text();
+				console.log(data);
+			} catch (err) {
+				console.error(err);
 			}
 		}
+
+		fetchData();
 	}, [count]);
 
 	return (
 		<>
 			<Box color="lightblue" children="name1"></Box>
 			<Box color="lightread"></Box>
-			<button onClick={() => setCount(count + 1)}></button>
+			<button onClick={() => setCount(count + 1)}> add count </button>
 		</>
 	);
 }
